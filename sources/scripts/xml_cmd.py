5c9e9a5bcf468a0e1f53d87e7de39c98018f430a
Laurent Belcour <laurent.belcour@inria.fr>
2013-09-24 14:54:22 +0200

    Working python command

--- sources/scripts/xml_cmd.py
+++ sources/scripts/xml_cmd.py
@@ -12,66 +12,97 @@ root = tree.getroot()
 cmds = [];
 
 def baseName():
-	return "./build/";
+	return './build/';
 #end
 
 def libName(name):
-	if(os.name == "posix"):
-		return "lib" + name + ".so";
-	elif(os.name == "nt"):
+	if(os.name == 'posix'):
+		return 'lib' + name + '.so';
+	elif(os.name == 'nt'):
 		return name + ".dll";
 	#endif
 #end
 
 ## Relative directories
-lib_dir = "";
-dat_dir = "";
+lib_dir = '';
+dat_dir = '';
+out_dir = '';
+
 
 ## Parse the configuration part of the XML file, this will set the global
 ## parameters such as the relative directories.
 ##
 def parseConfiguration(xmlNode):
+	global lib_dir, dat_dir, out_dir;
+
+	for param in xmlNode.findall('parameter'):
+
+		if(param.attrib['name'] == 'lib-dir'):
+			lib_dir = param.attrib['value'];
+
+		elif(param.attrib['name'] == 'dat-dir'):
+			dat_dir = param.attrib["value"];
+
+		elif(param.attrib['name'] == 'out-dir'):
+			dat_dir = param.attrib["value"];
+
+		#end
+	#end
 #end
 
-def parseFit(xmlNode):
 
-	cmd = "";
+def parseAction(xmlNode):
+	global lib_dir, dat_dir, out_dir;
+	cmd = '';
 
-	# Parse the command line arguments
-	for child in xmlNode:
-		
-		if(child.tag == "function"):
-			cmd += " --func " + baseName() + libName(child.attrib["plugin"]);
-			cmd += " --output " + child.attrib["file"];
+	inputNode = xmlNode.find('input');
+	if not (inputNode is None):
+		cmd += ' --input ' + inputNode.attrib['name'];
+	#end
+	
+	outputNode = xmlNode.find('output');
+	if not(outputNode is None):
+		cmd += ' --output ' + inputNode.attrib['name'];
+	#end
 
-		elif(child.tag == "data"):
-			cmd += " --input " + child.attrib["file"];
-			#cmd += " --data " + baseName() + libName(child.attrib["plugin"]);
-
-		elif(child.tag == "fitter"):
-			cmd += " --fitter " + baseName() + libName(child.attrib["plugin"]);
-		#endif
-
+	for plugin in xmlNode.findall('plugin'):
+		cmd += ' --' + plugin.attrib['type'];
+		cmd += ' ' + lib_dir + '/' + libName(plugin.attrib['name']);
+	#end
+	
+	for param in xmlNode.findall('parameter'):
+		cmd += ' --' + param.attrib['name'];
+		cmd += ' ' + param.attrib['value'];
 	#end
 
 	return cmd;
 #end
 
-# Command lines creation
-for child in root:
 
-	cmd = "";
+## Parse the configuration part of the file
+##
+conf = root.find("configuration");
+if conf is None:
+	print '<<PYTHON>> no configuration specified in the XML file';
+else:
+	parseConfiguration(conf);
+#end
 
-   if(child.tag == "cofiguration"):
-      parseConfiguration(child);
-   #end
 
-	if(child.tag == "fit"):
-		cmd += "./build/data2brdf";
-		cmd += parseFit(child);
+## Command lines creation
+##
+for child in root.findall('action'):
+
+	# Create the cmd string with the command name
+	cmd = lib_dir + '/' + child.attrib['name'];
+
+	# Parse the action
+	cmd += parseAction(child);
+
+	ret = os.system(cmd);
+	if(ret != 0):
+		print '<<PYTHON>> the action was not performed';
 	#end
-
-	os.system(cmd);
 
 
 #end
