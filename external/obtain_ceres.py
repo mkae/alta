8eecf7c73dda3efcac560b91a388e6e07eb36212
Laurent Belcour <laurent.belcour@inria.fr>
2013-10-11 22:32:46 +0200

    Full build of ceres from scons

--- external/obtain_ceres.py
+++ external/obtain_ceres.py
@@ -6,11 +6,16 @@ import shutil
 
 # Download GLOG
 obtain.obtain('GLOG v0.3.3', 'glog-0.3.3', 'http://google-glog.googlecode.com/files/glog-0.3.3.tar.gz', 'glog-0.3.3.tar.gz')
-if os.name == 'nt':
-	print '<<WARNING>> no automatic installation for this package'
+
+if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'glog'):
+	if os.name == 'nt':
+		print '<<WARNING>> no automatic installation for this package'
+	else:
+		print '<<INSTALL>> configure and build GLOG v0.3.3'
+		obtain.configure_build('glog-0.3.3')
+	#end
 else:
-	print '<<INSTALL>> configure and build GLOG v0.3.3'
-	obtain.configure_build('glog-0.3.3')
+	print '<<INSTALL>> GLOG already installed'
 #end
 
 # Download Eigen3
@@ -19,3 +24,18 @@ execfile('obtain_eigen.py')
 # Download CERES
 obtain.obtain('CERES v1.7.0', 'ceres-solver-1.7.0', 'http://ceres-solver.googlecode.com/files/ceres-solver-1.7.0.tar.gz', 'ceres-solver-1.7.0.tar.gz')
 print '<<WARNING>> CERES installation requires CMake. You need to run it yourself'
+
+if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'ceres'):
+	if os.name == 'nt':
+		print '<<WARNING>> no automatic installation for this package'
+	else:
+		print '<<INSTALL>> configure and build CERES'
+		os.chdir('.' + os.sep + 'ceres-solver-1.7.0')
+		build_dir = os.pardir + os.sep + 'build' + os.sep
+		ret = os.system('cmake -DGLOG_LIB=' + build_dir + 'lib -DGLOG_INCLUDE=' + build_dir + 'include -DGFLAGS=OFF -DEIGEN_INCLUDE=' + build_dir + 'include -DCMAKE_INSTALL_PREFIX=' + build_dir + ' .')
+		ret = os.system('make install')
+		os.chdir(os.pardir)
+	#end
+else:
+	print '<<INSTALL>> CERES already installed'
+#end
