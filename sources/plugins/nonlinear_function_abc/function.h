bab8c2e4f8c41a29fe1b2458b441aebb3c44ce2b
Laurent Belcour <laurent.belcour@inria.fr>
2013-11-14 14:26:02 +0100

    Adding the ABC reflectance model from Stover

---
+++ sources/plugins/nonlinear_function_abc/function.h
@@ -0,0 +1,71 @@
+#pragma once
+
+// Include STL
+#include <vector>
+#include <string>
+
+// Interface
+#include <core/function.h>
+#include <core/data.h>
+#include <core/fitter.h>
+#include <core/args.h>
+#include <core/common.h>
+
+/*! \brief  The ABC brdf model.
+ *  \details
+ *  Follows the implementation of Löw <i>et al.</i> [2012].
+ *
+ *  <h3>Plugin parameters</h3>
+ *
+ */
+class abc_function : public nonlinear_function
+{
+
+	public: // methods
+
+		abc_function()
+		{
+			setParametrization(params::COS_TH);
+			setDimX(1);
+		}
+
+		// Overload the function operator
+		virtual vec operator()(const vec& x) const ;
+		virtual vec value(const vec& x) const ;
+
+		//! \brief Load function specific files
+		virtual bool load(std::istream& in) ;
+
+		//! \brief Export function
+		virtual void save_call(std::ostream& out, const arguments& args) const;
+		virtual void save_body(std::ostream& out, const arguments& args) const;
+
+		//! \brief Boostrap the function by defining the diffuse term
+		//!
+		//! \details
+		virtual void bootstrap(const data* d, const arguments& args);
+
+		//! \brief Number of parameters to this non-linear function
+		virtual int nbParameters() const ;
+
+		//! \brief Get the vector of parameters for the function
+		virtual vec parameters() const ;
+
+		//! \brief get the min values for the parameters
+		virtual vec getParametersMin() const;
+
+		//! \brief Update the vector of parameters for the function
+		virtual void setParameters(const vec& p) ;
+
+		//! \brief Obtain the derivatives of the function with respect to the
+		//! parameters. 
+		virtual vec parametersJacobian(const vec& x) const ;
+
+		//! \brief Set the number of output dimensions
+		void setDimY(int nY);
+
+	private: // data
+
+		vec _a, _b, _c; // Lobes data
+} ;
+
--- sources/plugins/plugins.pro
+++ sources/plugins/plugins.pro
@@ -19,6 +19,7 @@ SUBDIRS  = \
 				nonlinear_fresnel_retroschlick			\
 				nonlinear_function_diffuse					\
 				nonlinear_function_microfacets			\
+				nonlinear_function_abc						\
 				nonlinear_function_beckmann				\
 				nonlinear_function_retrobeckmann			\
 				nonlinear_function_blinn					\
