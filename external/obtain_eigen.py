dca250cd2310311fa64aaf566f06007af600f656
Laurent Belcour <laurent.belcour@inria.fr>
2013-11-29 20:28:44 +0100

    Updating scons scripts

--- external/obtain_eigen.py
+++ external/obtain_eigen.py
@@ -9,4 +9,5 @@ obtain.obtain('Eigen v3.2.1', 'eigen-eigen-ffa86ffb5570', 'http://bitbucket.org/
 rep = 'build' + os.sep + 'include' + os.sep + 'Eigen'
 if not os.path.exists(rep):
 	shutil.copytree('eigen-eigen-ffa86ffb5570' + os.sep + 'Eigen', rep)
+	shutil.copytree('eigen-eigen-ffa86ffb5570' + os.sep + 'unsupported', rep)
 #end
