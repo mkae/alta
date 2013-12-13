6416e545b9b98f9662d624e7d225645c8151ee71
Laurent Belcour <laurent.belcour@inria.fr>
2013-12-13 10:42:10 +0100

    Adding automatic compilation of NlOpt

---
+++ external/obtain_nlopt.py
@@ -0,0 +1,19 @@
+import obtain
+import os
+import sys
+import shutil
+
+
+# Download NlOpt
+obtain.obtain('NlOpt', 'nlopt-2.4.1', 'http://ab-initio.mit.edu/nlopt/nlopt-2.4.1.tar.gz', 'nlopt-2.4.1.tqr.gz')
+
+if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'nlopt.hpp'):
+	if os.name == 'nt':
+		print '<<WARNING>> no automatic installation for this package'
+	else:
+		print '<<INSTALL>> configure and build Nlopt v2.4.1'
+		obtain.configure_build('nlopt-2.4.1')
+	#end
+else:
+	print '<<INSTALL>> NlOpt already installed'
+#end
