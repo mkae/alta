d8e8494962b7876bfca9a96d88c6a0b5b0dbf5d7
Romain Pacanowski <romain.pacanowski@inria.fr>
2015-02-10 15:39:00 +0100

    First implementation of this nonlinear plugin. The value and jacobian methods compile and work but more testing is needed

--- sources/plugins/nonlinear_shadowing_walter_smith/function.cpp
+++ sources/plugins/nonlinear_shadowing_walter_smith/function.cpp
@@ -17,7 +17,13 @@ ALTA_DLL_EXPORT function* provide_function()
   return new WalterSmith();
 }
 
-WalterSmith::WalterSmith() : SQRT_PI( 1.772453850905516 )
+WalterSmith::WalterSmith() 
+: LOCAL_EPSILON (1e-6),
+  FUNCTION_NAME_TOKEN("nonlinear_shadowing_walter_smith")
+#ifdef WALTER_SMITH_EXACT
+  ,
+SQRT_PI( 1.772453850905516 )
+#endif
 {
   setParametrization(params::CARTESIAN);
   setDimX(6);
@@ -27,91 +33,184 @@ WalterSmith::WalterSmith() : SQRT_PI( 1.772453850905516 )
 bool 
 WalterSmith::load(std::istream& in) 
 {
-  assert(0);
-  return false;
+  using namespace std;
+
+  // Parse line until the next comment
+  while(in.peek() != '#')
+  {
+    char line[256];
+    in.getline(line, 256);
+
+    // If we cross the end of the file, or the badbit is
+    // set, the file cannot be loaded
+    if(!in.good())
+      return false;
+  }
+
+  // Checking for the comment line #FUNC nonlinear_function_blinn
+  std::string token;
+  in >> token;
+  if(token != "#FUNC")
+  {
+      cerr << "<<ERROR>> parsing the stream. The #FUNC is not the next line defined." << endl;
+      
+      #ifndef DEBUG
+      std::cerr << "<<ERROR>> got \"" << token << "\"" << std::endl;
+      #endif      
+      return false;
+  }
+
+
+  in >> token;
+  if(token != FUNCTION_NAME_TOKEN)
+  {
+      std::cerr << "<<ERROR>> parsing the stream. function name is not the next token." << std::endl;
+  #ifndef DEBUG
+      std::cerr << "<<ERROR>> got \"" << token << "\"" << std::endl;
+  #endif
+      return false;
+  }
+
+  // alpha [double]
+  for(int i=0; i<dimY(); ++i)
+  {
+      in >> token >> _alpha[i];
+  }
+
+  #ifdef DEBUG
+  std::cout << "<<DEBUG>> load parameters " << parameters() << std::endl;
+  #endif
+  return true;
+
+
 }
 
 void 
 WalterSmith::save_call(std::ostream& out, const arguments& args) const
 {
-  assert(0);
+  
+  bool const is_alta   = !args.is_defined("export") || args["export"] == "alta";
+
+  if(is_alta)
+  {
+    out << "#FUNC " << FUNCTION_NAME_TOKEN << std::endl ;
+
+    for(int i=0; i< dimY(); ++i)
+    {
+      out << "alpha " << _alpha[i] << std::endl;
+    }
+    
+    out << std::endl;
+  }
+  else
+  {
+   out << "walter_smith(L, V, N, X, Y, vec3(";
+   for(int i=0; i<dimY(); ++i)
+   {
+     out << _alpha[i];
+     if(i < dimY()-1) 
+      { 
+        out << ", "; 
+      }
+   }
+
+   out << ") ) ";
+   
+  }
+
 }
 
 void 
 WalterSmith::save_body(std::ostream& out, const arguments& args) const
 {
-  assert(0);
+  bool is_shader = args["export"] == "shader" || args["export"] == "explorer";
+
+  if(is_shader)
+  {
+    out << "float shadow_approx_rational(float a )" << std::endl
+        << "{" << std::endl
+        << "\t if( a >= 1.6 ) " << std::endl
+        << "\t {"  << std::endl
+        << "\t\t return 1.0; " << std::endl
+        << "\t } else { "  << std::endl
+        << "\t\t " << std::endl
+        << "\t\t float num   = (3.535 + 2.181*a)*a;"  << std::endl
+        << "\t\t float denom = (2.267 + 2.577*a)*a + 1.0;" << std::endl
+        << "\t\t return num/denom;" << std::endl
+        << "\t }"
+        << "}" << std::endl << std::endl;
+
+    out << "vec3 walter_smith(vec3 L, vec3 V, vec3 N, vec3 X, vec3 Y, vec3 alpha )" << std::endl;
+    out << "{" << std::endl;
+    out << "\t vec3 H = normalize(L + V);" << std::endl;
+    out << "\t float NdotL  = max( dot(N,L), 0.0 );" << std::endl;
+    out << "\t float NdotV  = max( dot(N,V), 0.0 );" << std::endl;
+    out << "\t float dot_NH = max( dot(N,H), 0.0 );" << std::endl;
+    out << "\t float chi_v =  step( 0.0, dot_NH / NdotV ) ; " << std::endl;
+    out << "\t if( chi_v <= 0.0 ) { return vec3(0.0, 0.0, 0.0); }" << std::endl;
+    out << "\t float chi_l =  step( 0.0, dot_NH / NdotL ) ; " << std::endl;
+    out << "\t if( chi_l <= 0.0 ) { return vec3(0.0, 0.0, 0.0); }" << std::endl
+        << std::endl;
+
+    out << "\t float tan_theta_v = tan( acos( NdotV ) ); " << std::endl
+        << std::endl
+        << "\t vec3 geom_term_view(0.0); " << std::endl
+        << "\t geom_term_view[0] = shadow_approx_rational( 1.0 / (alpha[0]*tan_theta_v)) ;"<< std::endl
+        << "\t geom_term_view[1] = shadow_approx_rational( 1.0 / (alpha[1]*tan_theta_v)) ;"<< std::endl
+        << "\t geom_term_view[2] = shadow_approx_rational( 1.0 / (alpha[2]*tan_theta_v)) ;"<< std::endl
+        << std::endl
+        << "\t float tan_theta_l = tan( acos( NdotL ) ); " << std::endl
+        << "\t vec3 geom_term_light(0.0); " << std::endl    
+        << "\t geom_term_light[0] = shadow_approx_rational( 1.0 / (alpha[0]*tan_theta_l)) ;" << std::endl
+        << "\t geom_term_light[1] = shadow_approx_rational( 1.0 / (alpha[1]*tan_theta_l)) ;" << std::endl
+        << "\t geom_term_light[2] = shadow_approx_rational( 1.0 / (alpha[2]*tan_theta_l)) ;" << std::endl
+        << "\t return geom_term_view * geom_term_light;" << std::endl
+        << "}" << std::endl;
+
+  }
+
+
 }
 
 
 vec 
 WalterSmith::value(const vec& x) const
-{
-  assert(0);
+{   
   
-  double const LOCAL_EPSILON = 1e-6;
+   //Recover light and view directions from x in Cartesian Parametrisation
+  vec view(3);   vec light(3);  
+  //Normal Vector
+  vec n(3); vec half_vector(3);
 
+  getVectorsFromCartesianParam( x, view, light, n, half_vector);
 
-  //Recover light and view directions from x in Cartesian Parametrisation
-  vec view(3);
-  view[0] = x[0];   view[1] = x[1];   view[2] = x[2];
-  
-  vec light(3);
-  light[0] = x[3];  light[1] = x[4]; light[2] = x[5];
-
-  vec n(3); n[0] = 0.0; n[1] = 0.0; n[2] = 1.0;
-  
-
-  // Safe way to cpompute the half vector
-  vec half_vector(3);
-  half_vector = view + light;
-  double const length  = half_vector.norm();
-
-  if( length < LOCAL_EPSILON)
-  {
-    half_vector = n;    
-  }
-  else
-  {
-    half_vector /= length;   
-  }
-  
+  //Initialize result to zero by default
   vec res= vec::Zero( dimY() );
 
-
-  //Now Compute Shadowing Quantities
-  double const v_dot_h = dot(view, half_vector); 
-  double const v_dot_n = dot(view, n );
-  double const v_ratio = v_dot_h / v_dot_n;
-
-  double const light_dot_h = dot(light, half_vector); 
-  double const light_dot_n = dot(light, n );
-  double const light_ratio = light_dot_h / light_dot_n;
-
-  if (v_ratio <= 0 || light_ratio <=0 )  
+  if( partialShadowingTerm( x, view, light, n, half_vector) <= 0.0f )
   {
     return res;
   }
   else
   {
-    for( unsigned int i=0; i < dimY(); i++ )
+    //Now Compute Shadowing Quantities
+    for(unsigned int i=0; i < dimY(); i++)
     {
-      double const tan_theta_v = std::tan( std::acos( v_dot_n) );
+      //double const tan_theta_v = std::tan( std::acos( v_dot_n) );
+      double const tan_theta_v = std::sqrt(1.0 - view[2]*view[2]) / view[2];
       double const a_view = 1.0 / (_alpha[i] * tan_theta_v);
       double const geom_term_view = shadowTerm( a_view );
 
-      double const tan_theta_l= std::tan( std::acos( light_dot_n) );
+      //double const tan_theta_l= std::tan( std::acos( light_dot_n) );
+      double const tan_theta_l = std::sqrt(1 - light[2]*light[2]) / light[2];
       double const a_light= 1.0 / (_alpha[i] * tan_theta_l);
       double const geom_term_light = shadowTerm( a_light );
 
       res[i] = geom_term_light * geom_term_view;      
-      }
+    }
   }
-  
-  
-  
-  
 
+  return res;    
 }
 
 int  
@@ -123,40 +222,92 @@ WalterSmith::nbParameters() const
 vec  
 WalterSmith::parameters() const 
 {
-  vec res;
-  assert(0);
-  return res;  
+  vec res( _alpha.size() );
+  for(unsigned int i=0; i< _alpha.size(); i++)
+  {
+      res[i] = _alpha[i];
+  }
+
+  return res;
 }
 
 void 
 WalterSmith::setParameters(const vec& p) 
 {
-  assert(0);
+  for(unsigned int i=0; i<_alpha.size(); i++)
+  {
+    _alpha[i] = p[i];
+  }
 }
 
 vec  
 WalterSmith::getParametersMin() const
 {
-  vec res;
-  assert(0);
-  return res;
+  vec res( _alpha.size() );
+ 
+  for( unsigned int i=0; i < _alpha.size(); i++)
+  {
+    res[i] = LOCAL_EPSILON;
+  }
 
+  return res;
 }
 
 vec  
 WalterSmith::parametersJacobian(const vec& x) const 
 {
-  vec res;
-  assert(0);
-  return res;
+  //A 3x3 matrix for the Jacobian
+  vec jac = vec::Zero(dimY()*nbParameters());
 
+  vec view(3);   vec light(3);  
+  //Normal Vector
+  vec n(3); 
+  vec half_vector(3);
+
+  getVectorsFromCartesianParam( x, view, light, n, half_vector);
+
+  //First step compute Step functions to check that they are not equal to zero
+  if( partialShadowingTerm( x, view, light, n, half_vector) <= 0.0f )
+  {
+    return jac;
+  }
+
+
+  double const tan_theta_v = std::sqrt(1.0 - view[2]*view[2]) / view[2];
+  
+  double const tan_theta_l = std::sqrt(1 - light[2]*light[2]) / light[2];
+       
+  
+
+  // if they are not compute derivative of the alpha parameters
+  for(int i=0; i<dimY(); ++i)
+  {
+   for(int j=0; j<dimY(); ++j)
+   {
+      if(i == j)
+      {
+          double const a_theta_v = 1.0 / (_alpha[i] * tan_theta_v);
+          double const a_theta_l = 1.0 / (_alpha[i] * tan_theta_l);
+
+          jac[i*nbParameters() + j] =  partialDerivative(_alpha[i], tan_theta_v ) * shadowTerm( a_theta_l ) + shadowTerm( a_theta_v ) * partialDerivative(_alpha[i], tan_theta_l ) ;
+
+      }
+
+    }//end for j loop
+  }//end for i loop
+
+  return jac;
 }
+
 
 void 
 WalterSmith::bootstrap(const ptr<data> d, const arguments& args)
 {
-  assert(0);
-
+  
+  for(unsigned int i=0; i<_alpha.size(); i++) 
+  { 
+    _alpha[i] = 1.0; 
+  }
 }
 
 void 
@@ -169,9 +320,9 @@ WalterSmith:: setDimY(int nY)
 
 double  WalterSmith::shadowTerm( double a ) const 
 {
-  #define WALTER_SMITH_EXACT
+  //#define WALTER_SMITH_EXACT
   #ifdef WALTER_SMITH_EXACT
-  return 2.0 / ( 1 + erf(a) + std::exp(-a*a) / ( a * SQRT_PI));
+  return 2.0 / ( 1 + erf(a) + std::exp(-a*a) / ( a * SQRT_PI) );
   #else //THIS IS THE RATIONAL APPROXIMATION. FASTER BUT LESS ACCURATE
     if( a >= 1.6 )
     {
--- sources/plugins/nonlinear_shadowing_walter_smith/function.hpp
+++ sources/plugins/nonlinear_shadowing_walter_smith/function.hpp
@@ -73,14 +73,157 @@ class WalterSmith : public nonlinear_function
 
   private:
     double shadowTerm( double a ) const;
-  
+    inline void getVectorsFromCartesianParam( vec const & x,
+                                              vec & view,
+                                              vec & light, 
+                                              vec & normal,
+                                              vec & half_vector ) const;
+
+    
+    /**
+     * Returns 0 if x <= 0 and 1 in other case
+     * @param  x the positionat which the step function is to be evaluated
+     * @return   value of the step function 
+     */
+    inline float stepFunc( float x ) const ;
+
+    /**
+     * Compute the product of the two step functions  $\chi_1$ and $\chi_2$
+     * from the input vector $x$
+     * $\chi_1$ is defined as $\chi_1( \frac{v \cdot h}{v \cdot n})$
+     * $\chi_2$ is defined as $\chi_2( \frac{l \cdot h}{l \cdot n})$
+     *
+     * where $v$ is the view direction, $l$ the light direction and $n$
+     * is the normal of the surface
+     * 
+     * @param  x the input vector in the cartesian parametrization
+     * @param  view  the view vector
+     * @param  light the light vector
+     * @param  normal the normal of the surface
+     * @param  half_vector the half direction between the light and the view directions
+     * @return   the product of the two step function Chi_1 and Chi_2
+     */
+    inline float partialShadowingTerm( vec const & x,  
+                                       vec const & view,
+                                       vec const & light, 
+                                       vec const & normal,
+                                       vec const & half_vector ) const;
+
+
+    /**
+     * Compute the partial derivative of the Shadowing term, expressed
+     * as a rational function, according to the parameter alpha and 
+     * the given tangent value. 
+     *
+     * 
+     * 
+     * @param  alpha the value of the parameter
+     * @param  tan_value The tangent value which either tan( theta_view) or tan(theta_light)
+     * @return       the value of the partial derivate 
+     */
+    float partialDerivative( float alpha, float tangent_value) const;
 
   private: // parameters
 
   //TODO: CHECK that this is the correct parameter
   vec _alpha;
 
+  double const LOCAL_EPSILON;
+  std::string const  FUNCTION_NAME_TOKEN; 
+  
+  #ifdef WALTER_SMITH_EXACT
   double const SQRT_PI;
+  #endif
 
 } ;
+
+
+
+
+inline 
+void WalterSmith::getVectorsFromCartesianParam( vec const & x,
+                                                vec & view,
+                                                vec & light, 
+                                                vec & normal,
+                                                vec & half_vector ) const
+{
+  //Demultiplexing the view and light directions from x
+  view[0] = x[0];   view[1] = x[1];   view[2] = x[2];
+  
+  light[0] = x[3];  light[1] = x[4]; light[2] = x[5];
+
+  //Normal Vector
+   normal[0] = 0.0; normal[1] = 0.0; normal[2] = 1.0;
+  
+  // Safe way to compute the half vector
+  half_vector = view + light;
+  double const length  = half_vector.norm();
+
+  if( length < LOCAL_EPSILON )
+  {
+    half_vector = normal;    
+  }
+  else
+  {
+    half_vector /= length;   
+  }
+}
+
+
+inline 
+float  WalterSmith::stepFunc( float x ) const 
+{
+  if ( x <= 0.0 )
+  {
+    return 0.0f;
+  }
+  
+  return 1.0f;
+}
+
+
+inline float 
+WalterSmith::partialShadowingTerm(vec const & x,
+                                  vec const & view,
+                                  vec const & light, 
+                                  vec const & normal,
+                                  vec const & half_vector ) const
+{
+
+  //Now Compute Step functions 
+  double const v_dot_h = dot(view, half_vector); 
+  double const v_dot_n = dot(view, normal );
+
+  if( stepFunc( v_dot_h / v_dot_n ) <= 0.0 )
+  {
+    return 0.0f;
+  }
+
+  double const light_dot_h = dot(light, half_vector); 
+  double const light_dot_n = dot(light, normal );
+
+  if(stepFunc( light_dot_h / light_dot_n) <= 0.0 )  
+  {
+    return 0.0f;
+  }
+
+  return 1.0f;
+}
+ 
+
+inline 
+float
+WalterSmith::partialDerivative( float alpha, float tangent_value) const
+{
+  //Exported from Maple 
+  float const t1 = tangent_value;
+  float const t2 = alpha * t1;
+  float const t4 = alpha * alpha;
+  float const t5 = t1 * t1;
+  float const t6 = t4 * t5;
+  float t13 = 0.1000e4 * t6 + 0.2276e4 * t2 + 0.2577e4;
+  t13 *= t13;
+  return  -0.1e1 * t1 * (0.4362000e7 * t2 - 0.4145739e7 + 0.3535000e7 * t6) / t13;
+}
+
 
