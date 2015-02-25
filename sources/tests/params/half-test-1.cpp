c4f3b905f1d6538de012d4bbe7ee64ce97e202a4
Laurent Belcour <laurent.belcour@gmail.com>
2015-02-25 19:41:32 +0100

    Adding unitary tests to check the parametrizations. Those tests check the conversion from half <-> cartesian coordinates.
       * half test 1 checks for exact configurations.
       * half test 2 checks is resulting configurations are within
         the range predicted by the parametrization.

---
+++ sources/tests/params/half-test-1.cpp
@@ -0,0 +1,66 @@
+// ALTA includes
+#include <core/params.h>
+
+// STL includes
+#include <cmath>
+#include <iostream>
+
+bool closeTo(double a, double b) {
+	return abs(a-b) < 1.0E-10;
+}
+
+/* Test different configurations for the Half / Cartesian parametrization
+ * Returns: 0 is every test passes
+ *          n > 0 when n tests did no pass
+ */
+int main(int argc, char** argv) {
+
+	// Number of failed tests
+	int n = 0;
+
+	// Test the theta_h = 0 configuration
+	// This correspond to wi = R(wo), where R is the reflection with respect
+	// to the normal vector (0,0,1). 
+	for(int k=0; k<=90; ++k) {
+		const double theta = 0.5 * M_PI * double(k) / 90.0;
+		vec cart(6);
+		cart[0] = sin(theta);
+		cart[1] = 0.0;
+		cart[2] = cos(theta);
+		cart[3] =-sin(theta);
+		cart[4] = 0.0;
+		cart[5] = cos(theta);
+		vec x(3);
+
+		params::convert(&cart[0], params::CARTESIAN, params::RUSIN_TH_TD_PD, &x[0]);
+
+		if(!closeTo(x[0], 0.0) || !closeTo(x[1], theta) || !closeTo(x[2], 0.0)) {
+			std::cout << "<<ERROR>> configuration " <<  cart << " -> " << x << " failed" << std::endl;
+			++n;
+		}
+	}
+
+	// Test the configuration theta_H = 0 by converting half/diff vector
+	// to cartesian. This should convert to wi = R(wo) configuration.
+	for(int k=0; k<=90; ++k) {
+		const double theta = 0.5 * M_PI * double(k) / 90.0;
+		vec half(6);
+		half[0] = 0.0;
+		half[1] = theta;
+		half[2] = 0.0;
+		vec cart(6);
+
+		params::convert(&half[0], params::RUSIN_TH_TD_PD, params::CARTESIAN, &cart[0]);
+
+		if(!closeTo(cart[2], cos(theta)) || !closeTo(cart[2], cos(theta))) {
+			std::cout << "<<ERROR>> configuration " <<  half << " -> " << cart << " failed" << std::endl;
+			++n;
+		}
+	}
+
+	if(n > 0) {
+		std::cerr << "<<ERROR>> " << n << " tests of conversion CARTESIAN -> HALF failed" << std::endl;
+	}
+
+	return n;
+}
---
+++ sources/tests/params/half-test-2.cpp
@@ -0,0 +1,75 @@
+// ALTA includes
+#include <core/params.h>
+
+// STL includes
+#include <cmath>
+#include <iostream>
+
+bool closeTo(double a, double b) {
+	return abs(a-b) < 1.0E-10;
+}
+
+bool inRange(double x, double a, double b) {
+	return x >= a && x <= b;
+}
+
+/* Test different configurations for the Half / Cartesian parametrization
+ * Returns: 0 is every test passes
+ *          n > 0 when n tests did no pass
+ */
+int main(int argc, char** argv) {
+
+	// Number of failed tests
+	int n = 0;
+
+	const double PI2 = 0.5*M_PI;
+	const double d2r = M_PI / 180.0;
+	const int nphi   = 360;
+	const int ntheta = 90;
+	const int step   = 10;
+
+	// Sample the full hemisphere for both wi and wo. Check if the converted
+	// vector is within the range of the parametrization. Theta_h and Theta_d
+	// should be within [0..pi/2] and Phi_d should be within [0..2Pi].
+	for(int ti=0; ti<=ntheta; ti+=step) {
+		for(int pi=0; pi<=nphi; pi+=step) {
+
+			const double theta_i = d2r*double(ti);
+			const double phi_i   = d2r*double(pi);
+
+			#pragma omp parallel for
+			for(int to=0; to<=ntheta; to+=step) {
+				for(int po=0; po<=nphi; po+=step) {
+
+					const double theta_o = d2r*double(to);
+					const double phi_o   = d2r*double(po);
+
+					vec cart(6);
+					cart[0] = cos(phi_i)*sin(theta_i);
+					cart[1] = sin(phi_i)*sin(theta_i);
+					cart[2] = cos(theta_i);
+					cart[3] = cos(phi_o)*sin(theta_o);
+					cart[4] = sin(phi_o)*sin(theta_o);
+					cart[5] = cos(theta_o);
+					vec x(3);
+
+					params::convert(&cart[0], params::CARTESIAN, params::RUSIN_TH_PH_TD_PD, &x[0]);
+
+					#pragma omp critical (n)
+					if(!inRange(x[0], 0.0, PI2) || !inRange(x[2], 0.0, PI2)) {
+						std::cout << "<<ERROR>> configuration " <<  cart << " -> " << x << " failed" << std::endl;
+						++n;
+					}
+
+					std::cout << "<<INFO>> Check configuration " << po + nphi*(to + ntheta*(pi + nphi*ti)) << " / " << ntheta*ntheta*nphi*nphi << "      \r";
+				}
+			}
+		}
+	}
+
+	if(n > 0) {
+		std::cerr << "<<ERROR>> " << n << " tests of conversion CARTESIAN -> HALF failed" << std::endl;
+	}
+
+	return n;
+}
