236918a55363c7e75fce315324e4bf418e219426
Romain Pacanowski <romain.pacanowski@inria.fr>
2015-03-05 13:50:31 +0100

    Implementation of fit2stat. EMpty stub right now. Work in progress.

---
+++ sources/softs/fit2stat/fit2stat.cpp
@@ -0,0 +1,44 @@
+/* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
+
+   Copyright (C) 2013, 2014 Inria
+
+   This file is part of ALTA.
+
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0.  If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/.  */
+
+/*! \package fit2stats
+ *  \ingroup commands
+ *  \brief
+ *  This command takes at input the result of a BRDF fit as well as data and outputs statistics regarding
+ *  the fitting. 
+ *
+ *  \details
+ *  
+ *  <h3>Parameters</h3>
+ *  <ul>
+ */
+
+//STL
+#include <cstdlib>
+
+//ALTA
+#include <core/args.h>
+#include <core/data.h>
+#include <core/function.h>
+#include <core/fitter.h>
+#include <core/plugins_manager.h>
+
+
+int 
+main(int argc, char* argv[])
+{
+
+  arguments args(argc, argv) ;
+
+
+
+
+  return EXIT_SUCCESS;
+}
