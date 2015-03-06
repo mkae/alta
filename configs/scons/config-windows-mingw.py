d1e86373e029939f8f5522ba592f0d7e3a817ea1
Romain Pacanowski <romain.pacanowski@institutoptique.fr>
2015-03-06 15:47:18 +0100

    Canvas to test the compilation on Windows platform with mingw toolchain. Beware that to use this configuration you need to include the mingw tools in the SCons environment.

---
+++ configs/scons/config-windows-mingw.py
@@ -0,0 +1,100 @@
+import os, sys
+
+##----------------------------------------------------------------##
+## This file describes required and optional arguments to ALTA    ##
+## compilation. If you want to manualy tune the use of an already ##
+## present library, uncomment the according lines.                ##
+##----------------------------------------------------------------##
+
+
+## Compilators build flags
+##
+CXX            = 'g++'
+CCFLAGS        = ['-O2', '-g']
+
+# This is not the best place to past static linking since plugins need the -shared options 
+#LINKFLAGS			 = [ '-static-libgcc', '-static-libstdc++']
+
+## ALTA internal flags
+##
+CORE_LIB       = []
+SOFT_LIB       = ['core']
+PLUGIN_LIB     = ['core']
+
+
+## Python and boost-python library
+##
+PYTHON_INC    = ['']
+PYTHON_DIR    = []
+PYTHON_LIB    = ['boost_python', 'python2.7']
+
+
+## Eigen library
+##
+EIGEN_INC     = ['#external/build/include/Eigen']
+
+
+## OpenMP flags
+##
+OPENMP_FLAG   = ['-fopenmp']
+OPENMP_LIB    = ['gomp' ]
+
+
+## OpenEXR library
+##
+OPENEXR_INC    = ['']
+OPENEXR_DIR    = ['']
+
+
+## FLANN library
+##
+FLANN_INC    = ['']
+FLANN_DIR    = ['']
+FLANN_LIB    = ['flann']
+
+
+## QUADPROG library
+##
+## You have to specify the directory of the QuadProg library
+##
+QUADPROG_INC      = ['#external/quadprog++']
+QUADPROG_DIR      = ['#external/build/lib']
+QUADPROG_LIB      = ['quadprog++']
+
+
+## CERES library
+##
+## You have to specify both the directory of the CERES library
+## and the glog library
+##
+CERES_INC      = ['#external/build/include']
+CERES_DIR      = ['#external/build/lib']
+CERES_LIB      = ['ceres', 'miniglog']
+CERES_OPT_LIB  = ['gomp', 'lapack', 'blas']
+
+
+## NlOpt library
+##
+## You have to specify the directory of the NlOpt library
+##
+NLOPT_INC      = ['#external/build/include']
+NLOPT_DIR      = ['#external/build/lib']
+NLOPT_LIB      = ['nlopt']
+NLOPT_OPT_LIB  = []
+
+
+## coin IpOpt library
+##
+## You have to specify the directory of the IpOpt library
+##
+IPOPT_INC      = ['#external/build/include']
+IPOPT_DIR      = ['#external/build/lib']
+IPOPT_LIB      = ['ipopt']
+IPOPT_OPT_LIB  = []
+
+
+## MATLAB library and Engine
+##
+MATLAB_INC  = ['']
+MATLAB_DIR  = ['']
+MATLAB_LIB  = ['eng', 'mex','mat']
