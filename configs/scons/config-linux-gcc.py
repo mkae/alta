1c752032c5c3c8eac3f6f8b674f6dc14e17cec93
Laurent Belcour <laurent.belcour@gmail.com>
2015-03-24 09:04:01 +0100

    Correction of the configuration file on linux to fully use the pkg-config system.

--- configs/scons/config-linux-gcc.py
+++ configs/scons/config-linux-gcc.py
@@ -38,7 +38,7 @@ OPENMP_LIB   = ['gomp']
 ##
 #OPENEXR_INC    = ['/usr/include/OpenEXR']
 #OPENEXR_DIR    = ['/usr/lib/x86_64_linux-gnu']
-OPENEXR_LIB    = ['Half', 'IlmImf', 'IlmThread']
+#OPENEXR_LIB    = ['Half', 'IlmImf', 'IlmThread']
 
 
 ## FLANN library
