911338179b5cf35a099312f54aede180ef004262
Laurent Belcour <laurent.belcour@gmail.com>
2015-06-13 22:10:16 +0200

    [Fixe] Correction of the compilation of NlOpt according to what can be seen here: https://github.com/stevengj/nlopt/issues/10. However, it requires to put nlopt as a dynamic library.

--- external/obtain_ipopt.py
+++ external/obtain_ipopt.py
@@ -12,36 +12,34 @@ if sys.platform == 'darwin':
     flags = ' CXXFLAGS=\"--std=c++11\" CFLAGS=\"--std=c11\"'
 #end
 
-else:
-	# Download IpOpt.
-	if not os.path.exists('.' + os.sep + 'Ipopt-3.11.8.tgz'):
-		obtain.obtain('IpOpt v3.11.8', 'Ipopt-3.11.8',
-              'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.11.8.tgz', 'Ipopt-3.11.8.tgz',
-              '9f9b76075fbd9315286ea4d7c159c94cab4a4fb16122fb172b24910af5b5b75b')
+# Download IpOpt.
+if not os.path.exists('.' + os.sep + 'Ipopt-3.11.8.tgz'):
+	obtain.obtain('IpOpt v3.11.8', 'Ipopt-3.11.8',
+          'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.11.8.tgz', 'Ipopt-3.11.8.tgz',
+          '9f9b76075fbd9315286ea4d7c159c94cab4a4fb16122fb172b24910af5b5b75b')
 
-	if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'coin/IpIpoptNLP.hpp'):
-		if os.name == 'nt':
-			W.warn(obtain.AltaDependencyWarning, 'sorry, no automatic installation of IpOpt')
-		else:
-			C.progress_display('configuring and building Ipopt and its dependencies')
-
-			path  = os.getcwd()
-			third = path + os.sep + 'Ipopt-3.11.8' + os.sep + 'ThirdParty' + os.sep
-			os.chdir(third + 'Blas')
-			ret = os.system('./get.Blas')
-			os.chdir(third + 'Lapack')
-			ret = os.system('./get.Lapack')
-			os.chdir(third + 'ASL')
-			ret = os.system('./get.ASL')
-			os.chdir(third + 'Mumps')
-			ret = os.system('./get.Mumps')
-
-			os.chdir(path)
-			obtain.configure_build('Ipopt-3.11.8', '--enable-dependency-linking' + flags)
-		#end
+if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'coin/IpIpoptNLP.hpp'):
+	if os.name == 'nt':
+		W.warn(obtain.AltaDependencyWarning, 'sorry, no automatic installation of IpOpt')
 	else:
-		C.progress_display('IpOpt is already installed')
-		C.progress_display('if the plugins using IpOpt fail to build, \
-check its installation')
+		C.progress_display('configuring and building Ipopt and its dependencies')
+
+		path  = os.getcwd()
+		third = path + os.sep + 'Ipopt-3.11.8' + os.sep + 'ThirdParty' + os.sep
+		os.chdir(third + 'Blas')
+		ret = os.system('./get.Blas')
+		os.chdir(third + 'Lapack')
+		ret = os.system('./get.Lapack')
+		os.chdir(third + 'ASL')
+		ret = os.system('./get.ASL')
+		os.chdir(third + 'Mumps')
+		ret = os.system('./get.Mumps')
+
+		os.chdir(path)
+		obtain.configure_build('Ipopt-3.11.8', '--enable-dependency-linking' + flags)
 	#end
+else:
+	C.progress_display('IpOpt is already installed')
+	C.progress_display('if the plugins using IpOpt fail to build, \
+check its installation')
 #end
--- external/obtain_nlopt.py
+++ external/obtain_nlopt.py
@@ -3,24 +3,24 @@ import os
 import sys
 import shutil
 
-
-# Download NlOpt
-obtain.obtain('NlOpt', 'nlopt-2.4.1',
-              'http://ab-initio.mit.edu/nlopt/nlopt-2.4.1.tar.gz', 'nlopt-2.4.1.tar.gz',
-              'fe9ade54ed79c87f682540c34ad4e610ff32c9a43c52c6ea78cef6adcd5c1319')
-
 # On OSX, the c++11 flags are working. However, they are not on GNU/Linux
 flags = ''
 if sys.platform == 'darwin':
     flags = ' CXXFLAGS=\"--std=c++11\" CFLAGS=\"--std=c11\"'
 #end
 
+# Download NlOpt
+if not os.path.exists('.' + os.sep + 'nlopt-2.4.1.tar.gz'):
+   obtain.obtain('NlOpt', 'nlopt-2.4.1',
+              'http://ab-initio.mit.edu/nlopt/nlopt-2.4.1.tar.gz', 'nlopt-2.4.1.tar.gz',
+              'fe9ade54ed79c87f682540c34ad4e610ff32c9a43c52c6ea78cef6adcd5c1319')
+
 if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'nlopt.hpp'):
 	if os.name == 'nt':
 		print '<<WARNING>> no automatic installation for this package'
 	else:
 		print '<<INSTALL>> configure and build Nlopt v2.4.1'
-		obtain.configure_build('nlopt-2.4.1', '--enable-static=no --enable-shared=true --without-matlab --without-octave --without-python --without-guile  --with-pic=yes' + flags)
+		obtain.configure_build('nlopt-2.4.1', '--enable-shared --without-matlab --without-octave --without-python --without-guile  --with-pic' + flags)
 	#end
 else:
 	print '<<INSTALL>> NlOpt already installed'
