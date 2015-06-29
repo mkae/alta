bc0fe706cf5bd7d355270f133098db913b891dbe
Ludovic Courtès <ludovic.courtes@inria.fr>
2015-06-29 15:16:00 +0200

    tests: Move <tests.h> in its own name space.

--- sources/tests/tests.h
+++ sources/tests/tests.h
@@ -15,70 +15,73 @@
 
 #include <cmath>
 
-template<typename T>
-static bool close_to(const T a, const T b, const T epsilon = 1E-7)
+namespace alta { namespace tests
 {
-    return std::abs(a - b) < epsilon;
-}
+    template<typename T>
+    static bool close_to(const T a, const T b, const T epsilon = 1E-7)
+    {
+        return std::abs(a - b) < epsilon;
+    }
 
-template<typename T>
-static bool in_range(const T x, const T a, const T b)
-{
-	return x >= a && x <= b;
-}
+    template<typename T>
+    static bool in_range(const T x, const T a, const T b)
+    {
+      return x >= a && x <= b;
+    }
 
-template<typename T>
-static T degrees_to_radians(const T degrees)
-{
-    static const T d2r = M_PI / 180.;
-    return degrees * d2r;
-}
+    template<typename T>
+    static T degrees_to_radians(const T degrees)
+    {
+        static const T d2r = M_PI / 180.;
+        return degrees * d2r;
+    }
 
-template<typename T>
-class angle_range
-{
-public:
-    class angle_iterator
+    template<typename T>
+    class angle_range
     {
     public:
-        angle_iterator(const T start, const T step):
-        _value(start), _step(step) { }
-
-        bool operator!=(const angle_iterator& other) const
+        class angle_iterator
         {
-            return _value != other._value;
+        public:
+            angle_iterator(const T start, const T step):
+            _value(start), _step(step) { }
+
+            bool operator!=(const angle_iterator& other) const
+            {
+                return _value != other._value;
+            }
+
+            T operator*() const
+            {
+                return degrees_to_radians(_value);
+            }
+
+            const angle_iterator& operator++()
+            {
+                _value += _step;
+                return *this;
+            }
+
+        private:
+            T _value;
+            const T _step;
+        };
+
+        angle_range(T start, T end, T step)
+            : _start(start), _end(end), _step(step)
+        { }
+
+        angle_iterator begin()
+        {
+            return angle_iterator(_start, _step);
         }
 
-        T operator*() const
+        angle_iterator end()
         {
-            return degrees_to_radians(_value);
-        }
-
-        const angle_iterator& operator++()
-        {
-            _value += _step;
-            return *this;
+            return angle_iterator(_end, _step);
         }
 
     private:
-        T _value;
-        const T _step;
+        const T _start, _end, _step;
     };
-
-    angle_range(T start, T end, T step)
-        : _start(start), _end(end), _step(step)
-    { }
-
-    angle_iterator begin()
-    {
-        return angle_iterator(_start, _step);
-    }
-
-    angle_iterator end()
-    {
-        return angle_iterator(_end, _step);
-    }
-
-private:
-    const T _start, _end, _step;
-};
+}};
