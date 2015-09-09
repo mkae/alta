0013dd00b933b2845109976d1aa6021c6eb43b52
Laurent Belcour <laurent.belcour@umontreal.ca>
2015-09-09 20:34:58 +0200

    [Scons][Refactor] Converting indent to spaces.

--- external/obtain_ipopt.py
+++ external/obtain_ipopt.py
@@ -8,33 +8,33 @@ import SCons.SConf as C
 
 # Download IpOpt.
 if not os.path.exists('.' + os.sep + 'Ipopt-3.11.8.tgz'):
-	obtain.obtain('IpOpt v3.11.8', 'Ipopt-3.11.8',
+   obtain.obtain('IpOpt v3.11.8', 'Ipopt-3.11.8',
           'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.11.8.tgz', 'Ipopt-3.11.8.tgz',
           '9f9b76075fbd9315286ea4d7c159c94cab4a4fb16122fb172b24910af5b5b75b')
 
 if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'coin/IpIpoptNLP.hpp'):
-	if os.name == 'nt':
-		W.warn(obtain.AltaDependencyWarning, 'sorry, no automatic installation of IpOpt')
-	else:
-		C.progress_display('configuring and building Ipopt and its dependencies')
+   if os.name == 'nt':
+      W.warn(obtain.AltaDependencyWarning, 'sorry, no automatic installation of IpOpt')
+   else:
+      C.progress_display('configuring and building Ipopt and its dependencies')
 
-		path  = os.getcwd()
-		third = path + os.sep + 'Ipopt-3.11.8' + os.sep + 'ThirdParty' + os.sep
-		os.chdir(third + 'Blas')
-		ret = os.system('./get.Blas')
-		os.chdir(third + 'Lapack')
-		ret = os.system('./get.Lapack')
-		os.chdir(third + 'ASL')
-		ret = os.system('./get.ASL')
-		os.chdir(third + 'Mumps')
-		ret = os.system('./get.Mumps')
+      path  = os.getcwd()
+      third = path + os.sep + 'Ipopt-3.11.8' + os.sep + 'ThirdParty' + os.sep
+      os.chdir(third + 'Blas')
+      ret = os.system('./get.Blas')
+      os.chdir(third + 'Lapack')
+      ret = os.system('./get.Lapack')
+      os.chdir(third + 'ASL')
+      ret = os.system('./get.ASL')
+      os.chdir(third + 'Mumps')
+      ret = os.system('./get.Mumps')
 
-		os.chdir(path)
-		obtain.configure_build('Ipopt-3.11.8', '--enable-static --enable-dependency-linking')
-		#obtain.configure_build('Ipopt-3.11.8', '--enable-shared --enable-dependency-linking')
-	#end
+      os.chdir(path)
+      obtain.configure_build('Ipopt-3.11.8', '--enable-static --enable-dependency-linking')
+      #obtain.configure_build('Ipopt-3.11.8', '--enable-shared --enable-dependency-linking')
+   #end
 else:
-	C.progress_display('IpOpt is already installed')
-	C.progress_display('if the plugins using IpOpt fail to build, \
+   C.progress_display('IpOpt is already installed')
+   C.progress_display('if the plugins using IpOpt fail to build, \
 check its installation')
 #end
--- external/obtain_nlopt.py
+++ external/obtain_nlopt.py
@@ -11,13 +11,13 @@ if not os.path.exists('.' + os.sep + 'nlopt-2.4.1.tar.gz'):
               'fe9ade54ed79c87f682540c34ad4e610ff32c9a43c52c6ea78cef6adcd5c1319')
 
 if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'nlopt.hpp'):
-	if os.name == 'nt':
-		print '<<WARNING>> no automatic installation for this package'
-	else:
-		print '<<INSTALL>> configure and build Nlopt v2.4.1'
-		#obtain.configure_build('nlopt-2.4.1', '--enable-static --without-matlab --without-octave --without-python --without-guile')
-		obtain.configure_build('nlopt-2.4.1', '--enable-shared --without-matlab --without-octave --without-python --without-guile  --with-pic')
-	#end
+   if os.name == 'nt':
+      print '<<WARNING>> no automatic installation for this package'
+   else:
+      print '<<INSTALL>> configure and build Nlopt v2.4.1'
+      #obtain.configure_build('nlopt-2.4.1', '--enable-static --without-matlab --without-octave --without-python --without-guile')
+      obtain.configure_build('nlopt-2.4.1', '--enable-shared --without-matlab --without-octave --without-python --without-guile  --with-pic')
+   #end
 else:
-	print '<<INSTALL>> NlOpt already installed'
+   print '<<INSTALL>> NlOpt already installed'
 #end
