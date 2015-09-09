939e49e3ed87f744b190b16f55d5244c7b063305
Laurent Belcour <laurent.belcour@umontreal.ca>
2015-09-09 22:03:06 +0200

    [Scons][Debug] Correct the exception launched when an archive could not be downloaded. [Scons][Refactor] Most of the obtain_*.py are now using a clean code.

--- external/obtain_ceres.py
+++ external/obtain_ceres.py
@@ -7,19 +7,26 @@ import SCons.Warnings as W
 import SCons.SConf as C
 
 # Download GLOG
-obtain.obtain('GLOG v0.3.3', 'glog-0.3.3',
-              'http://google-glog.googlecode.com/files/glog-0.3.3.tar.gz', 'glog-0.3.3.tar.gz',
-              'fbf90c2285ba0561db7a40f8a4eefb9aa963e7d399bd450363e959929fe849d0')
+version   = '0.3.3'
+base      = 'glog'
+name      = 'GLOG v' + version
+directory = base + '-' + version
+url       = 'http://google-glog.googlecode.com/files/glog-' + version + '.tar.gz'
+filename  = 'glog-' + version + '.tar.gz'
+sha256    = 'fbf90c2285ba0561db7a40f8a4eefb9aa963e7d399bd450363e959929fe849d0'
+glog_obtained = obtain.obtain(name, directory, url, filename, sha256)
+glog_compiled = os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'glog')
 
-if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'glog'):
-   if os.name == 'nt':
-      W.warn(obtain.AltaDependencyWarning, 'sorry, no automatic installation of GLOG')
-   else:
-      exists_archive = os.path.exists('.' + os.sep + 'glog-0.3.3')
-      if sys.platform == 'darwin' and not exists_archive:
-         obtain.patch('glog-0.3.3/src/glog/stl_logging.h.in', 'glog.patch')
-      C.progress_display('configuring and building GLOG')
-      obtain.configure_build('glog-0.3.3', '--disable-shared --enable-static --with-pic')
+if glog_obtained and not glog_compiled:
+   # if os.name == 'nt':
+   #    W.warn(obtain.AltaDependencyWarning, 'sorry, no automatic installation of GLOG')
+   # else:
+   #    exists_archive = os.path.exists('.' + os.sep + directory)
+   if sys.platform == 'darwin':
+      obtain.patch('glog-0.3.3/src/glog/stl_logging.h.in', 'glog.patch')
+      
+   C.progress_display('configuring and building GLOG')
+   glog_compiled = obtain.configure_build(directory, '--disable-shared --enable-static --with-pic')
 else:
    C.progress_display('GLOG is already installed')
 
@@ -32,7 +39,7 @@ directory = base + '-' + version
 url       = 'http://ceres-solver.org/ceres-solver-' + version + '.tar.gz'
 filename  = 'ceres-solver-' + version + '.tar.gz'
 sha256    = '30ac0729249f908afe80cb6fd06ae6d037f25a60d9fac54f61344389adab9c1a'
-obtain.obtain(name, directory, url, filename, sha256)
+obtained  = obtain.obtain(name, directory, url, filename, sha256)
 
 ## Test for the presence of already compiled ceres version in
 ## the $ALTA/external/build directory. Then test for the
@@ -47,17 +54,16 @@ with open(os.devnull, 'w') as fnull:
                 'CMake is not installed but is needed to build CERES')
 
 
-if compile_test:
+if obtained and compile_test:
    C.progress_display('configuring and building CERES')
    os.chdir('.' + os.sep + 'ceres-solver-' + version)
    build_dir = os.pardir + os.sep + 'build' + os.sep
 
    glog_config = ''
-   if os.name == 'nt':
-      glog_config = '-DMINIGLOG=ON'
-   else:
+   if glog_compiled:
       glog_config = '-DGLOG_LIBRARY=' + build_dir + 'lib' + os.sep + 'libglog.a' + ' -DGLOG_INCLUDE_DIR=' + build_dir + 'include' + ' -DMINIGLOG=OFF'
-   #end
+   else:
+      glog_config = '-DMINIGLOG=ON'
 
    cmake_cmd = 'cmake -DBUILD_SHARED_LIBS=OFF ' + glog_config + ' include -DGFLAGS=OFF ' + '-DEIGEN_INCLUDE_DIR=' + build_dir + 'include -DCMAKE_INSTALL_PREFIX=' + build_dir + ' .' + ' -DBUILD_EXAMPLES=OFF ' + '-DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release'
    #cmake_cmd = 'cmake -DBUILD_SHARED_LIBS=ON ' + glog_config + ' include -DGFLAGS=OFF ' + '-DEIGEN_INCLUDE_DIR=' + build_dir + 'include -DCMAKE_INSTALL_PREFIX=' + build_dir + ' .' + ' -DBUILD_EXAMPLES=OFF ' + '-DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release'
