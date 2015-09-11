2e429992429b76a8ed505ca1f8194bb548e66d37
Ludovic Courtès <ludovic.courtes@inria.fr>
2015-09-11 17:07:37 +0200

    build: Move unit tests of the core to tests/core.
    Also whitespace-cleanup the files.

--- sources/tests/core/half-test-2.cpp
+++ sources/tests/core/half-test-2.cpp
@@ -38,30 +38,30 @@ static bool every(const std::function<bool (double)> &pred, const vec &v)
  */
 int main(int argc, char** argv) {
 
-	// Number of failed tests
-	int n = 0;
+  // Number of failed tests
+  int n = 0;
 
-	const int nphi   = 360;
-	const int ntheta = 90;
-	const int step   = 10;
+  const int nphi   = 360;
+  const int ntheta = 90;
+  const int step   = 10;
 
-	// Sample the full hemisphere for both ωi and ωo.  Check if the converted
-	// vector is within the range of the parametrization.  θh and θd should be
-	// within [0..π/2], and Φd should be within [-π..π].
+  // Sample the full hemisphere for both ωi and ωo.  Check if the converted
+  // vector is within the range of the parametrization.  θh and θd should be
+  // within [0..π/2], and Φd should be within [-π..π].
   for (auto&& theta_i : angle_range<double>(0, ntheta, step)) {
   for (auto&& phi_i : angle_range<double>(0, nphi, step)) {
 
-			#pragma omp parallel for
+      #pragma omp parallel for
       for (auto&& theta_o : angle_range<double>(0, ntheta, step)) {
       for (auto&& phi_o : angle_range<double>(0, nphi, step)) {
 
-					vec cart(6);
-					cart[0] = cos(phi_i)*sin(theta_i);
-					cart[1] = sin(phi_i)*sin(theta_i);
-					cart[2] = cos(theta_i);
-					cart[3] = cos(phi_o)*sin(theta_o);
-					cart[4] = sin(phi_o)*sin(theta_o);
-					cart[5] = cos(theta_o);
+          vec cart(6);
+          cart[0] = cos(phi_i)*sin(theta_i);
+          cart[1] = sin(phi_i)*sin(theta_i);
+          cart[2] = cos(theta_i);
+          cart[3] = cos(phi_o)*sin(theta_o);
+          cart[4] = sin(phi_o)*sin(theta_o);
+          cart[5] = cos(theta_o);
 
           // Are all the elements of CART positive?  IOW, check whether the
           // input coordinates are in the hemisphere.
@@ -69,28 +69,28 @@ int main(int argc, char** argv) {
               // No they aren't: skip this value.
               continue;
 
-					vec x(4);
-					params::convert(&cart[0], params::CARTESIAN, params::RUSIN_TH_PH_TD_PD, &x[0]);
+          vec x(4);
+          params::convert(&cart[0], params::CARTESIAN, params::RUSIN_TH_PH_TD_PD, &x[0]);
 
-					#pragma omp critical (n)
-					if(!in_range(x[0], 0.0, M_PI_2) || !in_range(x[2], 0.0, M_PI_2)
+          #pragma omp critical (n)
+          if(!in_range(x[0], 0.0, M_PI_2) || !in_range(x[2], 0.0, M_PI_2)
              || !in_range(x[3], -M_PI, M_PI)) {
-						std::cout << "<<ERROR>> configuration " <<  cart << " -> " << x << " failed" << std::endl;
-						++n;
-					}
+            std::cout << "<<ERROR>> configuration " <<  cart << " -> " << x << " failed" << std::endl;
+            ++n;
+          }
 
 #if 0
           std::cout << "<<INFO>> checked configuration "
                     << cart << "\r";
 #endif
-				}
-			}
-		}
-	}
+        }
+      }
+    }
+  }
 
-	if(n > 0) {
-		std::cerr << "<<ERROR>> " << n << " tests of conversion CARTESIAN -> HALF failed" << std::endl;
-	}
+  if(n > 0) {
+    std::cerr << "<<ERROR>> " << n << " tests of conversion CARTESIAN -> HALF failed" << std::endl;
+  }
 
-	return n;
+  return n;
 }
