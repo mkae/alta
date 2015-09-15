9809921bce604d6a4687515b62e2100422e5f828
Laurent Belcour <laurent.belcour@umontreal.ca>
2015-09-15 17:45:23 +0200

    [SCons][Update] Getting the latest IpOpt package.

--- external/obtain_ipopt.py
+++ external/obtain_ipopt.py
@@ -6,14 +6,23 @@ import subprocess
 import SCons.Warnings as W
 import SCons.SConf as C
 
+def getThirdParty(name) :
+   path  = os.getcwd()
+   third = path + os.sep + directory + os.sep + 'ThirdParty' + os.sep
+   os.chdir(third + name)
+   ret   = Popen(['./get.' + name]).wait()
+   if(ret != 0):
+      C.progress_display('Failed to get IpOpt third party \'' + name + '\'')
+   os.chdir(path)
+
 # Download IpOpt.
-version   = '3.8.11'
+version   = '3.12.4'
 base      = 'IpOpt'
 name      = 'IpOpt v' + version
 directory = base + '-' + version
 url       = 'http://www.coin-or.org/download/source/Ipopt/Ipopt-' + version + '.tgz'
 filename  = 'IpOpt-' + version + '.tgz'
-sha256    = '9f9b76075fbd9315286ea4d7c159c94cab4a4fb16122fb172b24910af5b5b75b'
+sha256    = '292afd952c25ec9fe6225041683dcbd3cb76e15a128764671927dbaf881c2e89'
 obtained  = obtain.obtain(name, directory, url, filename, sha256)
 
 compiled  = os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'coin/IpIpoptNLP.hpp')
@@ -24,20 +33,13 @@ if obtained and not compiled:
    else:
       C.progress_display('configuring and building ' + name + ' and its dependencies')
 
-      path  = os.getcwd()
-      third = path + os.sep + 'Ipopt-3.11.8' + os.sep + 'ThirdParty' + os.sep
-      os.chdir(third + 'Blas')
-      ret = os.system('./get.Blas')
-      os.chdir(third + 'Lapack')
-      ret = os.system('./get.Lapack')
-      os.chdir(third + 'ASL')
-      ret = os.system('./get.ASL')
-      os.chdir(third + 'Mumps')
-      ret = os.system('./get.Mumps')
+      getThirdParty('Blas')
+      getThirdParty('Lapack')
+      getThirdParty('ASL')
+      getThirdParty('Mumps')
 
       os.chdir(path)
-      obtain.configure_build('Ipopt-3.11.8', '--enable-static --enable-dependency-linking')
-
+      obtain.configure_build(directory, '--enable-static --enable-dependency-linking')
 else:
    C.progress_display('IpOpt is already installed')
    C.progress_display('if the plugins using IpOpt fail to build, check its installation')
