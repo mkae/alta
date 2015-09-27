46083a70623e1658df28fcf82c949a88c23f520a
Laurent Belcour <laurent.belcour@umontreal.ca>
2015-09-27 16:08:08 +0200

    [Refactoring] Moving the interpolation plugins to the directory data_interpolants.

---
+++ sources/plugins/data_interpolants/matlab.cpp
@@ -0,0 +1,223 @@
+/* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
+
+   Copyright (C) 2013 Inria
+
+   This file is part of ALTA.
+
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0.  If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/.  */
+
+// STL includes
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cassert>
+
+// ALTA includes
+#include <core/data.h>
+#include <core/vertical_segment.h>
+#include <core/common.h>
+#include <core/args.h>
+
+// Matlab includes
+#include <engine.h>
+
+mxArray *X, *Y, *x, *y;
+
+#define BUFFER_SIZE 10000
+char* output = new char[BUFFER_SIZE+1];
+
+
+/*! \ingroup datas
+ *  \ingroup plugins
+ *  \class data_interpolant_matlab
+ *  \brief This plugin provide an interpolation of \ref vertical_segment
+ *  data using Matlab `griddata` functions.
+ *
+ *  \details
+ *  This plugin provide an interpolation of \ref vertical_segment
+ *  data using Matlab `griddata` functions.
+ *
+ *  ### Requirements
+ *  This plugin requires the Matlab engine library to compile.
+ *
+ *  \author Laurent Belcour <laurent.belcour@umontreal.ca>
+ */
+class MatlabInterpolant : public data
+{
+	private: // data
+
+		// The data object used to load sparse points sets
+		ptr<data> _data;
+		Engine *ep;
+
+	public: // methods
+		MatlabInterpolant()
+		{
+			_data = new vertical_segment();
+
+			// Create matlab engine
+		#ifdef WIN32
+		    if (!(ep = engOpen(NULL)))
+		#else
+		    if (!(ep = engOpen("matlab -nosplash")))
+		#endif
+			{
+				std::cerr << "<ERROR>> can't start MATLAB engine" << std::endl ;
+			}
+
+			output[BUFFER_SIZE] = '\0';
+			engOutputBuffer(ep, output, BUFFER_SIZE) ;
+		}
+
+		virtual MatlabInterpolant()
+		{
+			delete _data;
+			delete[] output;
+
+			mxDestroyArray(x);
+			mxDestroyArray(y);
+			mxDestroyArray(X);
+			mxDestroyArray(Y);
+			engClose(ep);
+		}
+
+		// Load data from a file
+		virtual void load(const std::string& filename)
+		{
+			// Load the data
+			_data->load(filename);
+
+			// Copy the informations
+			setDimX(_data->dimX());
+			setDimY(_data->dimY());
+			setMin(_data->min());
+			setMax(_data->max());
+
+			Eigen::MatrixXd eX(_data->size(), dimX()*dimY());
+			Eigen::MatrixXd eY(_data->size(), dimY());
+			for(int i=0; i<_data->size(); ++i)
+			{
+				vec x = _data->get(i);
+
+				// For a point extract all its coordinates
+				for(int j=0; j<dimX(); ++j)
+				{
+					eX(i, j) =  x[j];
+				}
+
+				// Extract all its values
+				for(int k=0; k<dimY(); ++k)
+				{
+					eY(i, k) = x[dimX() + k];
+				}
+			}
+
+			// Create matrices
+			X = mxCreateDoubleMatrix(_data->size(), dimX(), mxREAL);
+			memcpy((void *)mxGetPr(X), (void *) eX.data(),  _data->size()*dimY()*dimX()*sizeof(double));
+			engPutVariable(ep, "X", X);
+
+			Y = mxCreateDoubleMatrix(_data->size(), dimY(), mxREAL);
+			memcpy((void *)mxGetPr(Y), (void *) eY.data(),  _data->size()*dimY()*sizeof(double));
+			engPutVariable(ep, "Y", Y);
+
+			x = mxCreateDoubleMatrix(1, dimX(), mxREAL);
+		}
+		virtual void load(const std::string& filename, const arguments&)
+		{
+			load(filename);
+		}
+
+		virtual void save(const std::string& filename) const
+		{
+			_data->save(filename);
+		}
+
+		// Acces to data
+		virtual vec get(int id) const
+		{
+			return _data->get(id);
+		}
+		inline virtual vec data_interpolant::operator[](int i) const
+		{
+			return get(i) ;
+		}
+
+		virtual void set(const vec& x)
+		{
+			NOT_IMPLEMENTED();
+		}
+		virtual void set(int i, const vec& x)
+		{
+			_data->set(i, x);
+		}
+
+		virtual vec value(const vec& ax) const
+		{
+			vec res = vec::Zero(dimY());
+
+			// Copy the input vector to matlab code
+			memcpy((void *)mxGetPr(x), (void *)&ax[0],  dimX()*sizeof(double));
+			engPutVariable(ep, "x", x);
+
+			std::stringstream cmd;
+
+			// Iterate over the output dimension
+			for(int i=0; i<dimY(); ++i)
+			{
+				// Evaluate the matlab routine
+				if(dimX() == 2)
+				{
+					cmd << "y(" << i+1 << ") = griddata(X(:,1), X(:,2), Y(:," << i+1 << "), x(1), x(2), 'cubic');";
+				}
+				else if(dimX() == 3)
+				{
+					cmd << "y(" << i+1 << ") = griddata(X(:,1), X(:,2), X(:,3), Y(:," << i+1 << "), x(1), x(2), x(3), 'cubic');";
+				}
+				else
+				{
+					cmd << "y(" << i+1 << ") = griddatan(X(:, 1:" << dimX() <<"), Y(:, " << i+1 << "), x, 'linear');";
+				}
+				engEvalString(ep, cmd.str().c_str());
+		#ifdef DEBUG
+				std::cout << output;
+		#endif
+			}
+
+			// Get results and copy it
+			y = engGetVariable(ep, "y") ;
+			double* y_val = (double*)mxGetData(y);
+			memcpy(&res[0], y_val, dimY()*sizeof(double));
+
+			// Fail safe: if the query point is outside of the convex hull of the
+			// data, rerun the algorithm using a nearest option.
+			if(isnan(res[0]))
+			{
+				for(int i=0; i<dimY(); ++i)
+				{
+					cmd.str(std::string());
+					cmd << "y("<< i+1 <<") = griddatan(X(:, 1:" << dimX() <<"), Y(:, " << i+1 << "), x, 'nearest');";
+					engEvalString(ep, cmd.str().c_str());
+				}
+				// Get results and copy it
+				y = engGetVariable(ep, "y") ;
+				double* y_val = (double*)mxGetData(y);
+				memcpy(&res[0], y_val, dimY()*sizeof(double));
+			}
+
+		   return res;
+		}
+
+		// Get data size, e.g. the number of samples to fit
+		virtual int size() const
+		{
+			return _data->size();
+		}
+};
+
+ALTA_DLL_EXPORT data* provide_data(const arguments&)
+{
+    return new MatlabInterpolant();
+}
---
+++ sources/plugins/data_interpolants/rbf.cpp
@@ -0,0 +1,243 @@
+/* ALTA --- Analysis of Bidirectional Reflectance Distribution Functions
+
+   Copyright (C) 2013, 2014 Inria
+
+   This file is part of ALTA.
+
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0.  If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/.  */
+
+#include <core/data.h>
+#include <core/vertical_segment.h>
+#include <core/common.h>
+#include <core/args.h>
+
+//#define USE_DELAUNAY
+#ifdef USE_DELAUNAY
+#include <CGAL/Cartesian_d.h>
+#include <CGAL/Homogeneous_d.h>
+#include <CGAL/Delaunay_d.h>
+
+
+typedef CGAL::Homogeneous_d<double> Kernel;
+typedef CGAL::Delaunay_d<Kernel> Delaunay_d;
+typedef Delaunay_d::Point_d Point;
+typedef Delaunay_d::Simplex_handle S_handle;
+typedef Delaunay_d::Point_const_iterator P_iter;
+
+Delaunay_d* D;
+int dD;
+#else
+#include <flann/flann.hpp>
+#endif
+
+/*! \ingroup datas
+ *  \ingroup plugins
+ *  \class data_interpolant_rbf
+ *  \brief This plugin provide an interpolation of \ref vertical_segment
+ *  data using *Radial Basis Function* interpolation.
+ *
+ *  \details
+ *  This interpolation plugin smooth sparse data measurments using radial
+ *  kernels. The interpolation is performed as:
+ *  <center>
+ *     \f$ \tilde{y}(\mathbf{x}) = {\sum_{\mathbf{x}_i \in \mathcal{B}(
+ *     \mathbf{x})} k(\mathbf{x} - \mathbf{x}_i) y_i \over \sum_{\mathbf{x}_i
+ *     \in \mathcal{B}(\mathbf{x})} k(\mathbf{x} - \mathbf{x}_i)}\f$
+ *  </center>
+ *
+ *  The kernel is set as \f$ k(\mathbf{d}) = {1 \over \epsilon + ||d||^2} \f$.
+ *
+ *  ### Requirements
+ *  This plugin requires the FLANN library to compile. On linux plateforms
+ *  it can be obtained by package `libflann-dev` and on OSX using the port
+ *  `flann`.
+ *
+ *  \author Laurent Belcour <laurent.belcour@umontreal.ca>
+ */
+class rbf_interpolant : public data
+{
+	private: // data
+		// The data object used to load sparse points sets
+		ptr<data> _data;
+
+		// Interpolation
+#ifndef USE_DELAUNAY
+		flann::Index< flann::L2<double> >* _kdtree;
+#endif
+		int _knn;
+
+	public:
+
+		rbf_interpolant() : _data(new vertical_segment())
+		{
+			_knn = 3;
+		}
+
+		virtual ~rbf_interpolant()
+		{
+		#ifndef USE_DELAUNAY
+			if(_kdtree != NULL)
+				delete _kdtree;
+		#else
+			if(D != NULL)
+				delete D;
+		#endif
+		}
+
+		// Load data from a file
+		virtual void load(const std::string& filename)
+		{
+			// Load the data
+			_data->load(filename);
+
+			// Copy the informations
+			setDimX(_data->dimX());
+			setDimY(_data->dimY());
+			setMin(_data->min());
+			setMax(_data->max());
+			setParametrization(_data->input_parametrization());
+			setParametrization(_data->output_parametrization());
+
+		#ifdef USE_DELAUNAY
+			dD = dimX()+dimY();
+			D  = new Delaunay_d(dD);
+			for(int i=0; i<_data->size(); ++i)
+			{
+				vec x = _data->get(i);
+
+				Point pt(dD, &x[0], &x[dD]);
+				D->insert(pt);
+			}
+
+			std::cout << "<<DEBUG>> number of points in the Delaunay triangulation: " << D->all_points().size() << std::endl;
+			std::cout << "<<DEBUG>> number of points in input: " << _data->size() << std::endl;
+		#else
+			// Update the KDtreee by inserting all points
+			double* _d = new double[dimX()*_data->size()];
+			flann::Matrix<double> pts(_d, _data->size(), dimX());
+			for(int i=0; i<_data->size(); ++i)
+			{
+				vec x = _data->get(i);
+				memcpy(pts[i], &x[0], dimX()*sizeof(double));
+			}
+			_kdtree = new flann::Index< flann::L2<double> >(pts, flann::KDTreeIndexParams(4));
+			_kdtree->buildIndex();
+		#endif
+		}
+		virtual void load(const std::string& filename, const arguments&)
+		{
+			load(filename);
+		}
+
+		virtual void save(const std::string& filename) const
+		{
+		}
+
+		// Acces to data
+		virtual vec get(int id) const
+		{
+			vec res(dimX() + dimY()) ;
+			return res ;
+		}
+		virtual vec operator[](int i) const
+		{
+			return get(i) ;
+		}
+
+		//! \todo Test this function
+		virtual void set(const vec& x)
+		{
+			NOT_IMPLEMENTED();
+		}
+		virtual void set(int i, const vec& x)
+		{
+			NOT_IMPLEMENTED();
+		}
+
+		virtual vec value(const vec& x) const
+		{
+			vec res = vec::Zero(dimY());
+
+		#ifndef USE_DELAUNAY
+			// Query point
+			vec xc(x);
+			flann::Matrix<double> pts(&xc[0], 1, dimX());
+			std::vector< std::vector<int> >    indices;
+			std::vector< std::vector<double> > dists;
+
+			_kdtree->knnSearch(pts, indices, dists, _knn, flann::SearchParams());
+
+			// Interpolate the value using the indices
+			double cum_dist = 0.0;
+			for(int i=0; i<indices[0].size(); ++i)
+			{
+				const int indice = indices[0][i];
+				vec y = _data->get(indice);
+
+		      const double kernel = 1.0/(1.0E-10 + dists[0][i]);
+
+				res      += kernel * y.tail(_nY);
+				cum_dist += kernel;
+			}
+			if(cum_dist > 0.0)
+			{
+				res /= cum_dist;
+			}
+		#else
+
+			Point pt_x(dD);
+			for(int j=0; j<dimX(); ++j) { pt_x[j] = x[j]; }
+			S_handle simplex = D->locate(pt_x);
+
+			if(simplex == Delaunay_d::Simplex_handle())
+			{
+				return res;
+			}
+
+			// Interpolate the value of the vertices of the Simplex to estimate
+			// the value of x
+			double cum_dist = 0.0;
+			for(int j=0; j<=dD; ++j)
+			{
+				Point y = D->point_of_simplex(simplex, j);
+
+				// Compute the distance between y and x
+				double dist = 0.0;
+				for(int i=0; i<dimX(); ++i) { dist += pow(y.homogeneous(i)-x[i], 2); }
+				dist = sqrt(dist);
+
+				// Interpolate the data
+				cum_dist += dist;
+				for(int i=0; i<dimY(); ++i)
+				{
+					res[i] += dist * y.homogeneous(dimX() + i);
+				}
+
+			}
+
+			if(cum_dist > 0.0)
+			{
+				for(int j=0; j<dimY(); ++j)
+				{
+					res[j] /= cum_dist;
+				}
+			}
+		#endif
+
+		   return res;
+		}
+
+		// Get data size, e.g. the number of samples to fit
+		virtual int size() const
+		{
+			assert(_data);
+			return _data->size();
+		}
+};
+
+ALTA_DLL_EXPORT data* provide_data(const arguments&)
+{
+    return new rbf_interpolant();
+}
