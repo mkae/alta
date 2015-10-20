mkae/alta

commit a44573e669d2c4857c00873d21891ecd81e1719a

[Scons][Debug] Correct the exception launched when an archive could not be downloaded. [Scons][Refactor] Most of the obtain_*.py are now using a clean code.

SConstruct
@@ -120,7 +120,8 @@ for var in [ 'PATH', 'CPATH', 'LIBRARY_PATH', 'PKG_CONFIG_PATH', 'TMP', 'TMPDIR'
 if len(envVars['PKG_CONFIG_PATH']) > 0:
 	envVars['PKG_CONFIG_PATH'] += ':'
 envVars['PKG_CONFIG_PATH'] += os.path.abspath('external' + os.sep + 'build' + os.sep + 'lib' + os.sep + 'pkgconfig')
-env = Environment(variables = vars, ENV = envVars )
+env = Environment(variables = vars, ENV = envVars)
+env.AppendUnique(CPPPATH = env['EIGEN_INC'])
 env['DL_EXTERNALS'] = GetOption('obtain_externals')
 
 

external/SConscript
@@ -44,7 +44,10 @@ else:
 	# if not conf.CheckCXXHeader('Eigen/Core'):
 	# 	execfile('obtain_eigen.py')
 	# env = conf.Finish()
-	execfile('obtain_eigen.py')
+	conf = Configure(env)
+	if not conf.CheckCXXHeader('Eigen/Core'):
+		execfile('obtain_eigen.py')
+	env = conf.Finish()
 
 	##
 	# The env variable is cloned for IpOpt, NlOpt and CERES to avoid

external/obtain.py
@@ -7,6 +7,7 @@ import hashlib
 import SCons.Errors
 import SCons.Warnings as W
 import SCons.SConf as C
+from subprocess import Popen, PIPE
 
 # Warning class to report stuff about our dependencies.
 class AltaDependencyWarning(W.WarningOnByDefault):
@@ -40,8 +41,14 @@ def check_integrity(filename, sha256):
                                         1, 1, filename)
 
 # Download from URL to FILENAME.
+# Return True if the package was successfully downloaded, else return False.
 def download(url, filename):
-   urllib.urlretrieve(url, filename)
+   try:
+      urllib.urlretrieve(url, filename)
+      return True
+   except IOError:
+      C.progress_display('Unable to download package, check your connection')
+      return False
 
 # Uncompress the archive
 def uncompress(filename):
@@ -56,23 +63,31 @@ def patch(filename, patch):
 def obtain(name, rep, url, filename, sha256):
    if not os.path.exists(rep):
       C.progress_display('obtaining ' + name)
-      if not os.path.exists(filename):
-         download(url, filename)
+
+      # Try to download the file if it exist. If an error happens, return
+      # False.
+      if not os.path.exists(filename) and not download(url, filename):
+         return False
+
       check_integrity(filename, sha256)
       uncompress(filename)
+      return True
+
    else:
       C.progress_display(name + ' source code is already available')
+      return True
 
 def configure_build(rep, options = ''):
    os.chdir(rep)
    ret = os.system('./configure -q --prefix=' + os.getcwd() + os.sep + os.pardir + os.sep + 'build ' + options)
    if ret != 0:
       print '<<ERROR>> unable to configure package'
-      exit
+      return False
 
    ret = os.system('make -s && make -s install')
    if ret != 0:
       print '<<ERROR>> unable to build & install package'
-      exit
+      return False
    
    os.chdir(os.pardir)
+   return True

external/obtain_ceres.py
@@ -7,19 +7,26 @@ import SCons.Warnings as W
 import SCons.SConf as C
 
 # Download GLOG
-obtain.obtain('GLOG v0.3.3', 'glog-0.3.3',
-              'http://google-glog.googlecode.com/files/glog-0.3.3.tar.gz', 'glog-0.3.3.tar.gz',
-              'fbf90c2285ba0561db7a40f8a4eefb9aa963e7d399bd450363e959929fe849d0')
+version   = '0.3.3'
+base      = 'glog'
+name      = 'GLOG v' + version
+directory = base + '-' + version
+url       = 'http://google-glog.googlecode.com/files/glog-' + version + '.tar.gz'
+filename  = 'glog-' + version + '.tar.gz'
+sha256    = 'fbf90c2285ba0561db7a40f8a4eefb9aa963e7d399bd450363e959929fe849d0'
+glog_obtained = obtain.obtain(name, directory, url, filename, sha256)
+glog_compiled = os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'glog')
 
-if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'glog'):
-   if os.name == 'nt':
-      W.warn(obtain.AltaDependencyWarning, 'sorry, no automatic installation of GLOG')
-   else:
-      exists_archive = os.path.exists('.' + os.sep + 'glog-0.3.3')
-      if sys.platform == 'darwin' and not exists_archive:
-         obtain.patch('glog-0.3.3/src/glog/stl_logging.h.in', 'glog.patch')
-      C.progress_display('configuring and building GLOG')
-      obtain.configure_build('glog-0.3.3', '--disable-shared --enable-static --with-pic')
+if glog_obtained and not glog_compiled:
+   # if os.name == 'nt':
+   #    W.warn(obtain.AltaDependencyWarning, 'sorry, no automatic installation of GLOG')
+   # else:
+   #    exists_archive = os.path.exists('.' + os.sep + directory)
+   if sys.platform == 'darwin':
+      obtain.patch('glog-0.3.3/src/glog/stl_logging.h.in', 'glog.patch')
+      
+   C.progress_display('configuring and building GLOG')
+   glog_compiled = obtain.configure_build(directory, '--disable-shared --enable-static --with-pic')
 else:
    C.progress_display('GLOG is already installed')
 
@@ -32,7 +39,7 @@ directory = base + '-' + version
 url       = 'http://ceres-solver.org/ceres-solver-' + version + '.tar.gz'
 filename  = 'ceres-solver-' + version + '.tar.gz'
 sha256    = '30ac0729249f908afe80cb6fd06ae6d037f25a60d9fac54f61344389adab9c1a'
-obtain.obtain(name, directory, url, filename, sha256)
+obtained  = obtain.obtain(name, directory, url, filename, sha256)
 
 ## Test for the presence of already compiled ceres version in
 ## the $ALTA/external/build directory. Then test for the
@@ -47,17 +54,16 @@ with open(os.devnull, 'w') as fnull:
                 'CMake is not installed but is needed to build CERES')
 
 
-if compile_test:
+if obtained and compile_test:
    C.progress_display('configuring and building CERES')
    os.chdir('.' + os.sep + 'ceres-solver-' + version)
    build_dir = os.pardir + os.sep + 'build' + os.sep
 
    glog_config = ''
-   if os.name == 'nt':
-      glog_config = '-DMINIGLOG=ON'
-   else:
+   if glog_compiled:
       glog_config = '-DGLOG_LIBRARY=' + build_dir + 'lib' + os.sep + 'libglog.a' + ' -DGLOG_INCLUDE_DIR=' + build_dir + 'include' + ' -DMINIGLOG=OFF'
-   #end
+   else:
+      glog_config = '-DMINIGLOG=ON'
 
    cmake_cmd = 'cmake -DBUILD_SHARED_LIBS=OFF ' + glog_config + ' include -DGFLAGS=OFF ' + '-DEIGEN_INCLUDE_DIR=' + build_dir + 'include -DCMAKE_INSTALL_PREFIX=' + build_dir + ' .' + ' -DBUILD_EXAMPLES=OFF ' + '-DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release'
    #cmake_cmd = 'cmake -DBUILD_SHARED_LIBS=ON ' + glog_config + ' include -DGFLAGS=OFF ' + '-DEIGEN_INCLUDE_DIR=' + build_dir + 'include -DCMAKE_INSTALL_PREFIX=' + build_dir + ' .' + ' -DBUILD_EXAMPLES=OFF ' + '-DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release'

external/obtain_eigen.py
@@ -2,19 +2,27 @@ import obtain
 import os
 import sys
 import shutil
+import SCons.Errors as Errors
 
 # Download Eigen3
+version   = '3.2.1'
+base      = 'Eigen'
+name      = 'Eigen v' + version
 directory = 'eigen-eigen-6b38706d90a9'
-obtain.obtain('Eigen v3.2.1', directory,
-              'http://bitbucket.org/eigen/eigen/get/3.2.1.tar.gz', 'eigen-3.2.1.tar.gz',
-              'fa9b1821608d8fd3b364ac9db62102b797364923fa0c74e6cbc4f9ba36c43e44')
+url       = 'http://bitbucket.org/eigen/eigen/get/' + version + '.tar.gz'
+filename  = 'eigen-' + version + '.tar.gz'
+sha256    = 'fa9b1821608d8fd3b364ac9db62102b797364923fa0c74e6cbc4f9ba36c43e44'
+obtained  = obtain.obtain(name, directory, url, filename, sha256)
 
-rep = 'build' + os.sep + 'include' + os.sep + 'Eigen'
-if not os.path.exists(rep):
-   shutil.copytree(directory + os.sep + 'Eigen', rep)
-#end
+if obtained:
+   rep = 'build' + os.sep + 'include' + os.sep + 'Eigen'
+   if not os.path.exists(rep):
+      shutil.copytree(directory + os.sep + 'Eigen', rep)
 
-unsup_rep = 'build' + os.sep + 'include' + os.sep + 'unsupported'
-if not os.path.exists(unsup_rep):
-   shutil.copytree(directory + os.sep + 'unsupported', unsup_rep)
-#end
+   unsup_rep = 'build' + os.sep + 'include' + os.sep + 'unsupported'
+   if not os.path.exists(unsup_rep):
+      shutil.copytree(directory + os.sep + 'unsupported', unsup_rep)
+
+else:
+   msg = "Unable to obtain " + name
+   raise Errors.BuildError(msg, 1, 1, filename)

external/obtain_ipopt.py
@@ -7,12 +7,18 @@ import SCons.Warnings as W
 import SCons.SConf as C
 
 # Download IpOpt.
-if not os.path.exists('.' + os.sep + 'Ipopt-3.11.8.tgz'):
-   obtain.obtain('IpOpt v3.11.8', 'Ipopt-3.11.8',
-          'http://www.coin-or.org/download/source/Ipopt/Ipopt-3.11.8.tgz', 'Ipopt-3.11.8.tgz',
-          '9f9b76075fbd9315286ea4d7c159c94cab4a4fb16122fb172b24910af5b5b75b')
+version   = '3.8.11'
+base      = 'IpOpt'
+name      = 'IpOpt v' + version
+directory = base + '-' + version
+url       = 'http://www.coin-or.org/download/source/Ipopt/Ipopt-' + version + '.tgz'
+filename  = 'IpOpt-' + version + '.tgz'
+sha256    = '9f9b76075fbd9315286ea4d7c159c94cab4a4fb16122fb172b24910af5b5b75b'
+obtained  = obtain.obtain(name, directory, url, filename, sha256)
 
-if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'coin/IpIpoptNLP.hpp'):
+compiled  = os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'coin/IpIpoptNLP.hpp')
+
+if obtained and not compiled:
    if os.name == 'nt':
       W.warn(obtain.AltaDependencyWarning, 'sorry, no automatic installation of IpOpt')
    else:

external/obtain_nlopt.py
@@ -3,14 +3,19 @@ import os
 import sys
 import shutil
 
+# Download NlOpt.
+version   = '2.4.1'
+base      = 'NlOpt'
+name      = 'NlOpt v' + version
+directory = base + '-' + version
+url       = 'http://ab-initio.mit.edu/nlopt/nlopt-' + version + '.tar.gz'
+filename  = 'nlopt-' + version + '.tar.gz'
+sha256    = 'fe9ade54ed79c87f682540c34ad4e610ff32c9a43c52c6ea78cef6adcd5c1319'
+obtained  = obtain.obtain(name, directory, url, filename, sha256)
 
-# Download NlOpt
-if not os.path.exists('.' + os.sep + 'nlopt-2.4.1.tar.gz'):
-   obtain.obtain('NlOpt', 'nlopt-2.4.1',
-              'http://ab-initio.mit.edu/nlopt/nlopt-2.4.1.tar.gz', 'nlopt-2.4.1.tar.gz',
-              'fe9ade54ed79c87f682540c34ad4e610ff32c9a43c52c6ea78cef6adcd5c1319')
-
-if not os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'nlopt.hpp'):
+compiled  = os.path.exists('.' + os.sep + 'build' + os.sep + 'include' + os.sep + 'nlopt.hpp')
+ 
+if obtained and not compiled:
    if os.name == 'nt':
       print '<<WARNING>> no automatic installation for this package'
    else:

commit 8b8b31076916ec1e34b8cd195787c1ae2eca3e59

build: Use the system's Eigen when available.
Fixes a regression introduced in a44573e6.

SConstruct
@@ -125,7 +125,6 @@ if len(envVars['PKG_CONFIG_PATH']) > 0:
 	envVars['PKG_CONFIG_PATH'] += ':'
 envVars['PKG_CONFIG_PATH'] += os.path.abspath('external' + os.sep + 'build' + os.sep + 'lib' + os.sep + 'pkgconfig')
 env = Environment(variables = vars, ENV = envVars)
-env.AppendUnique(CPPPATH = env['EIGEN_INC'])
 env['DL_EXTERNALS'] = GetOption('obtain_externals')
 
 

external/SConscript
@@ -35,9 +35,11 @@ else:
    ##
    #   Eigen dependency
    ##
-   conf = Configure(env)
+   conf = Configure(env, custom_tests = { 'CheckPKG' : CheckPKG })
+   if conf.CheckPKG('eigen3'):
+     env.ParseConfig('pkg-config --cflags --libs eigen3')
    if not conf.CheckCXXHeader('Eigen/Core'):
-      execfile('obtain_eigen.py')
+     execfile('obtain_eigen.py')
    env = conf.Finish()
 
    ##
@@ -99,4 +101,4 @@ if env['CC'] != 'cl':
 
 env.StaticLibrary(target = qp_target, source = qp_sources,
                   CCFLAGS = qp_ccflags,
-                  CPPPATH = ['build' + os.sep + 'include'])
+                  CPPPATH = env['CPPPATH'] + ['build' + os.sep + 'include'])