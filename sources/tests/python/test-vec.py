0705aa8a136d02f9e3b055042a238fcd2ac3b757
Laurent Belcour <laurent.belcour@umontreal.ca>
2015-11-24 03:54:02 -0500

    [Test][Python] Improving the test for python vec wrapper

--- sources/tests/python/test-vec.py
+++ sources/tests/python/test-vec.py
@@ -1,27 +1,35 @@
 import alta
 
-fail = False
+fail = 0
 
 # Init the vector using the
 x = alta.vec([1.0, 1.0, 1.0])
 if x[0] != 1.0:
-    fail = True
+    fail += 1
 
 # Vector arithmetic
 y = x + x
 z = x - x
 if y[0] != 2.0 and z[0] != 0.0:
-    fail = True
+    fail += 1
 
 # Testing str(vec)
 print "Testing 'str' method on variable 'y': " + str(y)
 
 # Testing the length of the vector
 if len(y) != 3:
-    fail = True
+    fail += 1
+
+# Convertion to a list
+l = list(y)
+if len(l) != len(y):
+    fail += 1
+for i in range(0,len(y)):
+    if l[i] != y[i]:
+        fail += 1
 
 # Print the test result
-if fail:
+if fail > 0:
     print "Testing python 'vec' interface failed!"
     exit(1)
 else:
